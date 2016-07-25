236bdb69bfa724224f2e0ac546885257b8494da6
chrischeungmipow <Chris@zhangguorongs-MacBook-Pro.local>
2016-07-25 19:03:24 +0800

    Some snippets of Playbulb

---
+++ FoursExample/FoursExample/EffectMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  EffectMainViewController.h
+//  PlaybulbColor
+//
+//  Created by Stephen Yip on 14-8-6.
+//  Copyright (c) 2014年 Interface Implemenation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EffectMainViewController : UIViewController
+
+@end
---
+++ FoursExample/FoursExample/EffectMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  EffectMainViewController.m
+//  PlaybulbColor
+//
+//  Created by Stephen Yip on 14-8-6.
+//  Copyright (c) 2014年 Interface Implemenation. All rights reserved.
+//
+
+#import "EffectMainViewController.h"
+
+@interface EffectMainViewController ()
+
+@end
+
+@implementation EffectMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
